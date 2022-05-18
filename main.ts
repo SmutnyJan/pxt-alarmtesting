8cc44070dcb954671ac6b74b47737d08f4b2bd4d
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-05-18 19:48:19 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+alarm.onAlarm(function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+radio.onReceivedString(function (receivedString) {
+    vysledek = alarm.receiveBroadcast(receivedString)
+    if (vysledek == true) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . . . .
+            `)
+    }
+})
+let vysledek = false
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . # # # .
+    # # # # #
+    . . . . .
+    `)
+radio.setGroup(5)
